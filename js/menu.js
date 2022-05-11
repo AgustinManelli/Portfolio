aceaa031dd4bbdc84f55025da2b59a0b83c35f85
Agustin Manelli <agustinmanelli@hotmail.com>
2022-05-10 22:50:06 -0300

    Cambio generales sobre header (botones) y correciones.

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,3 @@
-/*addEventListener("DOMContentLoaded", () => {
-    const nav__menu = document.querySelector(".nav__menu");
-    nav__menu.addEventListener("click", () => {
-      const nav__ul = document.querySelector(".nav__ul");
-        nav__ul.classList.toggle("show");
-      
-    });
-});*/
-
-
 addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector(".burger");
     burger.addEventListener("click", () => {
